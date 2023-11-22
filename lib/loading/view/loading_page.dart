be2085616420fd2dabbb05d95974774c43f85cf6
Robert Rosborg <rosborg.robert@gmail.com>
2023-11-22 22:49:33 +0100

    Added loading screen

---
+++ lib/loading/view/loading_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+/// A loading page that is shown while retrieving data from a repository.
+class LoadingPage extends StatelessWidget {
+  const LoadingPage({super.key});
+
+  static Route<void> route() {
+    return MaterialPageRoute<void>(builder: (_) => const LoadingPage());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: CircularProgressIndicator()),
+    );
+  }
+}
