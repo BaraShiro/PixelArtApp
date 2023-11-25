5cf1a3809087481c5ff9d42767e068a1906d1c03
Robert Rosborg <rosborg.robert@gmail.com>
2023-11-25 13:22:38 +0100

    Added more helper functions

--- lib/art_listing/model/helpers.dart
+++ lib/art_listing/model/helpers.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 
 import 'package:flutter/foundation.dart';
 import 'package:pixelart_shared/pixelart_shared.dart';
+import 'package:uuid/uuid.dart';
 
 Future<Image> pixelArtToImage({required PixelArt pixelArt}) async {
   final Completer<Image> completer = Completer<Image>();
@@ -12,23 +13,20 @@ Future<Image> pixelArtToImage({required PixelArt pixelArt}) async {
       green: 0,
       blue: 255,
       alpha: 255,
-      placedBy: Participant(
-          id: "debug",
-          name: "debug"
-      )
-  );
+      placedBy: Participant(id: "debug", name: "debug"));
 
   for (int y = 0; y < pixelArt.height; y++) {
     for (var x = 0; x < pixelArt.width; x++) {
       int colOffset = y * pixelArt.width * 4;
       int rowOffset = x * 4;
       Pixel pixel;
-      if(y >= pixelArt.pixelMatrix.length || x >= pixelArt.pixelMatrix[y].length) {
+      if (y >= pixelArt.pixelMatrix.length ||
+          x >= pixelArt.pixelMatrix[y].length) {
         pixel = debug;
       } else {
-        pixel  = pixelArt.pixelMatrix[y][x];
+        pixel = pixelArt.pixelMatrix[y][x];
       }
-      
+
       pixelList[colOffset + rowOffset + 0] = pixel.red.toUnsigned(8);
       pixelList[colOffset + rowOffset + 1] = pixel.green.toUnsigned(8);
       pixelList[colOffset + rowOffset + 2] = pixel.blue.toUnsigned(8);
@@ -37,14 +35,51 @@ Future<Image> pixelArtToImage({required PixelArt pixelArt}) async {
   }
 
   decodeImageFromPixels(
-      pixelList,
-      pixelArt.width,
-      pixelArt.height,
-      PixelFormat.rgba8888,
-      (Image image) {
-        completer.complete(image);
-      },
+    pixelList,
+    pixelArt.width,
+    pixelArt.height,
+    PixelFormat.rgba8888,
+    (Image image) {
+      completer.complete(image);
+    },
   );
 
   return completer.future;
+}
+
+const Participant anon = Participant(id: 'anonymous', name: 'Anonymous');
+
+Participant constructNewUser({required String userName}) {
+  return Participant(id: const Uuid().v4(), name: userName);
+}
+
+const Pixel _background = Pixel(
+  red: 255,
+  green: 255,
+  blue: 255,
+  alpha: 0,
+  placedBy: anon,
+);
+
+PixelArt constructNewPixelArt(
+    {required Participant creator,
+    required String name,
+    required String description,
+    required int width,
+    required int height}) {
+  int validateDimension(int dimension) => dimension < 16
+      ? 16
+      : dimension > 128
+          ? 128
+          : dimension;
+
+  return PixelArt(
+      id: const Uuid().v4(),
+      name: name,
+      description: description,
+      width: validateDimension(width),
+      height: validateDimension(height),
+      editors: [creator],
+      pixelMatrix: List<List<Pixel>>.filled(
+          height, List<Pixel>.filled(width, _background)));
 }
