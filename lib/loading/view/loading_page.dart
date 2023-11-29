ff21fa5049d69a2b8c2b0ad9a55e4dd4b5bf676d
Robert Rosborg <rosborg.robert@gmail.com>
2023-11-29 23:11:53 +0100

    Simplified loading page

--- lib/loading/view/loading_page.dart
+++ lib/loading/view/loading_page.dart
@@ -10,8 +10,8 @@ class LoadingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: CircularProgressIndicator()),
+    return const Center(
+          child: CircularProgressIndicator()
     );
   }
 }
