93b4012d6457a19ac5f0e2ccf692283a4a3487fc
Robert Rosborg <rosborg.robert@gmail.com>
2023-11-30 16:26:22 +0100

    Added nes palette

---
+++ lib/palette/model/nes_palette.dart
@@ -0,0 +1,118 @@
+import 'dart:ui';
+
+const List<List<Color>> nesPalette = [
+  teals,
+  blueGreens,
+  greens,
+  yellowGreens,
+  yellows,
+  oranges,
+  reds,
+  pinks,
+  purples,
+  deepPurples,
+  darkBlues,
+  lightBlues,
+  grays,
+  blackWhiteTransparent,
+];
+
+const List<Color> teals = [
+    Color.fromARGB(255, 0, 64, 78),
+    Color.fromARGB(255, 0, 124, 142),
+    Color.fromARGB(255, 72, 206, 223),
+    Color.fromARGB(255, 181, 236, 243)
+];
+
+const List<Color> blueGreens = [
+    Color.fromARGB(255, 0, 79, 8),
+    Color.fromARGB(255, 0, 144, 50),
+    Color.fromARGB(255, 69, 225, 130),
+    Color.fromARGB(255, 180, 243, 205)
+];
+
+const List<Color> greens = [
+    Color.fromARGB(255, 0, 82, 0),
+    Color.fromARGB(255, 13, 148, 0),
+    Color.fromARGB(255, 93, 229, 48),
+    Color.fromARGB(255, 190, 245, 171)
+];
+
+const List<Color> yellowGreens = [
+    Color.fromARGB(255, 12, 73, 0),
+    Color.fromARGB(255, 56, 135, 0),
+    Color.fromARGB(255, 137, 217, 0),
+    Color.fromARGB(255, 208, 240, 151)
+];
+
+const List<Color> yellows = [
+    Color.fromARGB(255, 52, 53, 0),
+    Color.fromARGB(255, 108, 110, 0),
+    Color.fromARGB(255, 189, 191, 0),
+    Color.fromARGB(255, 229, 230, 149)
+];
+
+const List<Color> oranges = [
+    Color.fromARGB(255, 87, 29, 0),
+    Color.fromARGB(255, 153, 79, 0),
+    Color.fromARGB(255, 235, 159, 35),
+    Color.fromARGB(255, 247, 217, 166)
+];
+
+const List<Color> reds =  [
+    Color.fromARGB(255, 108, 7, 0),
+    Color.fromARGB(255, 181, 50, 32),
+    Color.fromARGB(255, 254, 130, 112),
+    Color.fromARGB(255, 254, 205, 198)
+];
+
+const List<Color> pinks = [
+    Color.fromARGB(255, 110, 0, 64),
+    Color.fromARGB(255, 184, 30, 124),
+    Color.fromARGB(255, 254, 110, 205),
+    Color.fromARGB(255, 254, 197, 235)
+];
+
+const List<Color> purples = [
+    Color.fromARGB(255, 92, 0, 126),
+    Color.fromARGB(255, 161, 27, 205),
+    Color.fromARGB(255, 243, 106, 254),
+    Color.fromARGB(255, 251, 195, 254)
+];
+
+const List<Color> deepPurples= [
+    Color.fromARGB(255, 59, 0, 164),
+    Color.fromARGB(255, 118, 39, 255),
+    Color.fromARGB(255, 199, 119, 254),
+    Color.fromARGB(255, 233, 200, 254)
+];
+
+const List<Color> darkBlues = [
+    Color.fromARGB(255, 20, 18, 168),
+    Color.fromARGB(255, 66, 64, 254),
+    Color.fromARGB(255, 147, 144, 254),
+    Color.fromARGB(255, 212, 211, 254)
+];
+
+const List<Color> lightBlues = [
+    Color.fromARGB(255, 0, 42, 136),
+    Color.fromARGB(255, 21, 95, 218),
+    Color.fromARGB(255, 100, 176, 254),
+    Color.fromARGB(255, 193, 224, 254)
+];
+
+const List<Color> grays = [
+    Color.fromARGB(255, 79, 79, 79),
+    Color.fromARGB(255, 102, 102, 102),
+    Color.fromARGB(255, 174, 174, 174),
+    Color.fromARGB(255, 184, 184, 184)
+];
+
+const List<Color> blackWhiteTransparent = [
+  Color.fromARGB(255, 0, 0, 0),
+  Color.fromARGB(255, 255, 255, 255),
+  Color.fromARGB(0, 255, 255, 255),
+  Color.fromARGB(0, 255, 255, 255)
+];
+
+
