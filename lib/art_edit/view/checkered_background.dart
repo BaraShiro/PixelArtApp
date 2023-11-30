716de62deaeb0420ea31ace21f3a49f6379be43e
Robert Rosborg <rosborg.robert@gmail.com>
2023-11-30 16:59:04 +0100

    CheckeredBackground now takes an argument for repetitions

--- lib/art_edit/view/checkered_background.dart
+++ lib/art_edit/view/checkered_background.dart
@@ -2,17 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:patterns_canvas/patterns_canvas.dart';
 
 class CheckeredBackground extends Decoration {
+  final int repetitions;
+  const CheckeredBackground({this.repetitions = 32});
+
   @override
   BoxPainter createBoxPainter([VoidCallback? onChanged]) {
-    return _CheckeredPainter();
+    return _CheckeredPainter(repetitions: repetitions);
   }
 }
 
 class _CheckeredPainter extends BoxPainter {
+  final int repetitions;
+  _CheckeredPainter({required this.repetitions});
+
   @override
   void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
     final Rect bounds = offset & configuration.size!;
-    const Pattern pattern = Checkers(bgColor: Colors.black12, fgColor: Colors.white, featuresCount: 32);
+    Pattern pattern = Checkers(bgColor: Colors.black12, fgColor: Colors.white, featuresCount: repetitions);
 
     pattern.paintOnRect(canvas, configuration.size!, bounds);
   }
